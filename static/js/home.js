05465df6b14ed1136fd8e02a509779f412c27e40
theSinner <amirajorloo@gmail.com>
2014-08-05 12:18:11 +0430

    blur effect fixed

--- static/js/home.js
+++ static/js/home.js
@@ -1,54 +1,36 @@
 $(document).ready(imReady);
-
+function startThePage()
+{
+	$("html, body").animate({ scrollTop: $(window).height()}, "slow");
+}
 function imReady()
 {
-	opactityOffset=10/$(window).height();
+	opactityOffset=5/$(window).height();
 	blurOpacity=0;
-	prevPos=-1;
-	var heightOffset=$(window).height()-$('#main-text-plate').height()-$('#market-plate').height()-$(window).height()/12;
-	heightOffset/=2;
+	
 	$('#main-page').css({'height':$(window).height().toString()+'px'});
 	$('#main-page-blur').css({'height':$(window).height().toString()+'px'});
 	$('#main-page-blur').css({'margin-top':(-1*$(window).height()).toString()+'px'});
 
 	$('#main-title-plate').css({'height':$(window).height().toString()+'px'});
 	$('#main-title-plate').css({'margin-top':(-1*$(window).height()).toString()+'px'});
-
-	var heightOffset=$(window).height()-$('#main-text-plate').height()-$('#market-plate').height()-$(window).height()/12;
+	var heightOffset=$(window).height()-$('#main-text-plate').height()-$('#startButton').height()-$(window).height()/12;
 	heightOffset/=2;
 
 	$('#main-text-plate').css({'margin-top':heightOffset.toString()+'px','margin-bottom':heightOffset.toString()+'px'})
+	$('#startButtonP').css({'margin-top':(($('#startButton').height()-$('#startButtonP').height())/2).toString()+'px'});
+	$('#startButton').click(startThePage);
 	$(window).scroll(function()
 	{
 		//console.log($(document).scrollTop())
-		if($(document).scrollTop()<0)
+		if($(document).scrollTop()<$(window).height())
 		{
-
-		}
-		else if($(document).scrollTop()<10)
-		{
-			$('#main-page-blur').css({'opacity':'0'})
-		}
-		else if($(document).scrollTop()<$(window).height())
-		{
-			//console.log(prevPos)
-			//console.log($(document).scrollTop())
-			if(prevPos<$(document).scrollTop())
-			{
-				prevPos=$(document).scrollTop();
-				blurOpacity+=opactityOffset;
-				if(blurOpacity>1)
-					blurOpacity=1;
-				$('#main-page-blur').css({'opacity':blurOpacity.toString()})
-			}
-			else
-			{
-				prevPos=$(document).scrollTop();
-				blurOpacity-=opactityOffset;
-				if(blurOpacity<0)
-					blurOpacity=0;
-				$('#main-page-blur').css({'opacity':blurOpacity.toString()})
-			}
+			blurOpacity=opactityOffset*$(document).scrollTop();
+			if(blurOpacity>1)
+				blurOpacity=1;
+			else if(blurOpacity<0)
+				blurOpacity=0;
+			$('#main-page-blur').css({'opacity':blurOpacity.toString()})
 		}
 	});
 }
