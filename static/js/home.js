c5dd3f3c84d7b7f24c229e176b799ba303634136
theSinner <amirajorloo@gmail.com>
2014-08-06 13:19:18 +0430

    show images when resize from small to large!

--- static/js/home.js
+++ static/js/home.js
@@ -12,6 +12,29 @@ function goToBlog()
 	var win = window.open('http://blog.mirad.ir', '_blank');
 	win.focus();
 }
+function resizing()
+{
+	opactityOffset=5/$(window).height();
+	blurOpacity=0;
+	
+	$('#main-page').css({'height':$(window).height().toString()+'px'});
+	$('#main-page-blur').css({'height':$(window).height().toString()+'px'});
+	$('#main-page-blur').css({'margin-top':(-1*$(window).height()).toString()+'px'});
+
+	$('#main-title-plate').css({'height':$(window).height().toString()+'px'});
+	$('#main-title-plate').css({'margin-top':(-1*$(window).height()).toString()+'px'});
+	var heightOffset=$(window).height()-$('#main-text-plate').outerHeight()-$('#startButton').outerHeight()-$(window).height()/100;
+	heightOffset/=2;
+	$('.developerImagePlate').css({'height':$('.developerDetail').height().toString()+'px'});
+
+	$('#main-text-plate').css({'margin-top':heightOffset.toString()+'px','margin-bottom':heightOffset.toString()+'px'})
+	$('#startButtonP').css({'margin-top':(($('#startButton').height()-$('#startButtonP').height())/2).toString()+'px'});
+	$('#goTopP').css({'margin-top':(($('#goTop').height()-$('#goTopP').height())/2).toString()+'px'});
+
+	heightOffset=$('.developerDetailSmall').height();
+	$('.developerDetailSmall').css({'padding-top':(($('.developersImageSmall').width()-heightOffset)/2).toString()+'px'});
+	
+}
 function imReady()
 {
 	opactityOffset=5/$(window).height();
